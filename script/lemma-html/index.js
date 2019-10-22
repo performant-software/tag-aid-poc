af3d3f4c2e88a2bdbde97f97538109a4ad11b2c3
Nick Laiacona <nick@performantsoftware.com>
2019-10-22 14:50:33 -0400

    output lemma and translation texts

--- script/lemma-html/index.js
+++ script/lemma-html/index.js
@@ -19,6 +19,56 @@ class LemmaHTML {
     //     svg.write(dotresult.stdout)
   }
 
+  lemmaToHTML( witReadings, annos ) {
+    let textElements = [], annotationElements = []
+
+    // index the annos by beginning reading ID 
+    const annoMap = {}
+    for( const anno of annos ) {
+      let beginID
+      for( const link of anno.links ) {
+        if( !beginID && link.type === 'BEGIN' ) {
+          beginID = link.target
+        }
+      }
+      annoMap[beginID] = anno
+    }
+
+    for (const [idx, rdg] of witReadings.entries()) {
+      // Get the span text
+      const rdgtext = rdg.normal_form ? rdg.normal_form : rdg.text
+      // Push on any space that needs to go before the span
+      if (idx > 0 && !witReadings[idx-1].join_next && !rdg.join_prior) {
+        textElements.push(' ')
+      }
+
+      // If an annotation starts here, begin link span and write the annotation
+      let currentAnno = annoMap[rdg.id]
+      if( currentAnno ) {
+        annotationElements.push(`<span>${currentAnno.properties.text}</span>`)
+      }
+
+      // Now push on the span for this reading
+      textElements.push( `<span id='text-${rdg.id}' key=${rdg.id}>${rdgtext}</span>`)
+    }
+
+    const text = `<html><head></head><body><p>${textElements.join('')}</p></body></html>`
+    const translation = `<html><head></head><body><p>${annotationElements.join(' ')}</p></body></html>`
+    return { text, translation }
+  }
+
+  getWitnessReadings(sigil, rdglist) {
+		const filterCondition = sigil === 'Lemma text'
+			? (r) => r.is_lemma && !r.is_start && !r.is_end
+			: (r) => r.witnesses.includes(sigil) && !r.is_start && !r.is_end;
+		const witReadings = rdglist.filter(filterCondition);
+		witReadings.sort((first, second) => first.rank - second.rank)
+		return {
+			sigil: sigil,
+			readings: witReadings
+		};
+	}
+
   async collectSectionData( options, section, outdir, auth ) {
     const sectiondir = `${outdir}/${section.id}`
 
@@ -26,17 +76,20 @@ class LemmaHTML {
       fs.mkdirSync(sectiondir)
     }
 
-
     const baseURL = `${options.repository}/tradition/${options.tradition_id}/section/${section.id}`
     this.collectSectionDOT(baseURL,sectiondir,auth)
 
-    // load the annotations
-    
-
     const r = await axios.get(`${baseURL}/readings`, {auth})
-    const reading = r.data
-    
-    //fs.writeFileSync( `${sectiondir}/readings.json`, JSON.stringify(reading) )
+    const readings = r.data
+    const lemmaReadings = this.getWitnessReadings("Lemma text", readings)
+
+    const annoResponse = await axios.get( `${baseURL}/annotations`, {auth, params: {label: 'TRANSLATION'}})
+    const annos = annoResponse.data
+
+    const docs = this.lemmaToHTML( lemmaReadings.readings, annos )
+
+    fs.writeFileSync( `${sectiondir}/lemmaText.html`, docs.text )
+    fs.writeFileSync( `${sectiondir}/translation.html`, docs.translation )
   }
   
   async collectTraditionData( options, auth ) {
@@ -64,13 +117,13 @@ class LemmaHTML {
   }
 
   loadConfig() {
-      const configJSON = fs.readFileSync(`lemma-html-config.json`, "utf8");
+      const configJSON = fs.readFileSync(`script/lemma-html/lemma-html-config.json`, "utf8");
       return JSON.parse(configJSON);
   }
 
  async main() {   
     const config = this.loadConfig()
-    this.collectTraditionData(config.options, config.auth)
+    await this.collectTraditionData(config.options, config.auth)
     console.log('Done!')
   }
 
